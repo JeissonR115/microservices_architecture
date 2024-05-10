2d2f51c85f79a8ccf2131a6afebd7d67bc6bdfdf
jeissonR115 <jeissonfeliper@gmail.com>
2024-05-10 17:25:28 -0500

    add gitignore

--- services/insert/main.js
+++ services/insert/main.js
@@ -1,1 +1,20 @@
-console.log("MicroServicio Insert");
+import { Server } from "./src/Server.js";
+
+export class Main {
+    static startServer() {
+        const collections = {
+            sensorData: "sensorData",
+            users: "users",
+            UsuariosM:"UsuariosM"
+        };
+        const server = new Server({
+            port:  3000 ,
+            url: 'mongodb+srv://ambidata2024:ambidata2024**@ambidata.vn0dlbx.mongodb.net/' ,
+            dbName:  'ambidata',// Definir el nombre de la base de datos.
+            collectionList: collections,// Definir una lista de colecciones dentro de la base de datos.
+            collectionName: collections.UsuariosM,// En este caso, se utiliza la colección 'sensorData' como la colección principal.
+        });
+        server.start();
+    }
+}
+Main.startServer();
